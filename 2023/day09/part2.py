ff70da884ffe13942f346d1ab3abdda7cad0598f
Stephen Jones <stephen@fergsusonjones.ca>
2023-12-09 13:07:21 -0500

    2023 Day 8 + ignore text files

---
+++ 2023/day09/part2.py
@@ -0,0 +1,25 @@
+import timeit
+
+from shared import SAMPLE, PUZZLE, read_lines, find_all_numbers_in_string, find_all_digits_in_string
+
+
+def get_diffs(numbers):
+    if set(numbers) == {0}:
+        return 0
+    diffs = []
+    for i in range(1, len(numbers)):
+        diffs.append(numbers[i] - numbers[i - 1])
+    return numbers[0] - get_diffs(diffs)
+
+
+def do_puzzle():
+    finals = []
+    for line in read_lines(PUZZLE):
+        numbers = find_all_numbers_in_string(line)
+        finals.append(get_diffs(numbers))
+    print(sum(finals))
+
+
+if __name__ == "__main__":
+    exec_time = timeit.timeit(do_puzzle, number=1) * 10 ** 3
+    print(f"{exec_time:.03f}ms")
