79b8b458aa70d2924231fd1ba7ad960e21588216
Stephen Jones <stephen@fergsusonjones.ca>
2023-12-13 23:53:36 -0500

    2023 Day 13 part 1

---
+++ 2023/Day13/part1.py
@@ -0,0 +1,47 @@
+import timeit
+
+from shared import (
+    SAMPLE,
+    PUZZLE,
+    read_lines,
+    find_all_numbers_in_string,
+    find_all_digits_in_string,
+)
+
+
+def do_puzzle():
+    patterns = []
+    pattern = []
+    for l in read_lines(SAMPLE):
+        if not l:
+            patterns.append(pattern)
+            pattern = []
+            continue
+        pattern.append(l)
+    patterns.append(pattern)
+    left = 0
+    above = 0
+    for rows in patterns:
+        columns = list(zip(*rows))
+        above += find_reflection(rows)
+        left += find_reflection(columns)
+    print(f"{100*above+left}")
+
+
+def find_reflection(rows):
+    for idx, r in enumerate(rows[1:], start=1):
+        if rows[idx - 1] == r:
+            reflection = True
+            for x in range(min(idx, len(rows) - idx)):
+                if rows[idx - 1 - x] == rows[idx + x]:
+                    continue
+                reflection = False
+                break
+            if reflection:
+                return idx
+    return 0
+
+
+if __name__ == "__main__":
+    exec_time = timeit.timeit(do_puzzle, number=1) * 10**3
+    print(f"{exec_time:.03f}ms")
